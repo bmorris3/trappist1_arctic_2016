a88c9f879313a4df70b9b833aad9d45bca3ca424
Brett Morris <brettmorris21@gmail.com>
2017-07-13 19:34:12 -0400

    Ok this time it's actually the last update before reevaluating

---
+++ trappist1b_20160918.py
@@ -0,0 +1,57 @@
+import os
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+from toolkit import (generate_master_flat_and_dark, photometry,
+                     PhotometryResults, PCA_light_curve, params_b)
+
+# Image paths
+image_paths = sorted(glob('/Users/bmmorris/data/UT160918/Trappist_science.*.fits'))#[:-130]
+dark_30s_paths = glob('/Users/bmmorris/data/UT160711/dark_quad_30.*.fits')
+night_flat_paths = glob('/Users/bmmorris/data/UT160711/nightskyflatz.*.fits')
+master_flat_path = 'outputs/masterflat.fits'
+master_dark_path = 'outputs/masterdark.fits'
+
+# Photometry settings
+target_centroid = np.array([[952], [906]])
+comparison_flux_threshold = 0.2
+aperture_radii = np.arange(30, 40, 2)
+centroid_stamp_half_width = 25
+psf_stddev_init = 10
+aperture_annulus_radius = 10
+transit_parameters = params_b
+
+
+output_path = 'outputs/trappist1b_20160918.npz'
+force_recompute_photometry = False  # True
+
+# Calculate master dark/flat:
+if not os.path.exists(master_dark_path) or not os.path.exists(master_flat_path):
+    print('Calculating master flat:')
+    generate_master_flat_and_dark(night_flat_paths, dark_30s_paths,
+                                  master_flat_path, master_dark_path)
+
+# Do photometry:
+
+if not os.path.exists(output_path) or force_recompute_photometry:
+    print('Calculating photometry:')
+    phot_results = photometry(image_paths, master_dark_path, master_flat_path,
+                              target_centroid, comparison_flux_threshold,
+                              aperture_radii, centroid_stamp_half_width,
+                              psf_stddev_init, aperture_annulus_radius,
+                              output_path)
+
+else:
+    phot_results = PhotometryResults.load(output_path)
+
+print('Calculating PCA...')
+light_curve = PCA_light_curve(phot_results, transit_parameters, plots=True)
+
+
+
+plt.figure()
+plt.plot(phot_results.times, light_curve, 'k.')
+plt.xlabel('Time [JD]')
+plt.ylabel('Flux')
+plt.show()
---
+++ trappist1c_20160619.py
@@ -0,0 +1,55 @@
+import os
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+from toolkit import (generate_master_dark, photometry, transit_model_c,
+                     PhotometryResults, PCA_light_curve, params_c)
+
+# Image paths
+image_paths = sorted(glob('/Users/bmmorris/data/UT160619/trappist-1.*.fits'))[:-10]
+dark_paths = glob('/Users/bmmorris/data/UT160619/dark-45s.*.fits')
+night_flat_paths = glob('/Users/bmmorris/data/UT160711/nightskyflatz.*.fits')
+master_flat_path = 'outputs/masterflat_binning3x3.fits' #'outputs/masterflat_placebo.fits'
+master_dark_path = 'outputs/masterdark_binning3x3.fits'
+
+# Photometry settings
+target_centroid = np.array([[633], [579]])
+comparison_flux_threshold = 0.2
+aperture_radii = np.arange(7, 18)
+centroid_stamp_half_width = 5
+psf_stddev_init = 2
+aperture_annulus_radius = 10
+transit_parameters = params_c
+
+
+output_path = 'outputs/trappist1c_20160619.npz'
+force_recompute_photometry = False  # True
+
+# Calculate master dark/flat:
+if not os.path.exists(master_dark_path):
+    generate_master_dark(dark_paths, master_dark_path)
+
+# Do photometry:
+if not os.path.exists(output_path) or force_recompute_photometry:
+    print('Calculating photometry:')
+    phot_results = photometry(image_paths, master_dark_path, master_flat_path,
+                              target_centroid, comparison_flux_threshold,
+                              aperture_radii, centroid_stamp_half_width,
+                              psf_stddev_init, aperture_annulus_radius,
+                              output_path)
+
+else:
+    phot_results = PhotometryResults.load(output_path)
+
+print('Calculating PCA...')
+light_curve = PCA_light_curve(phot_results, transit_parameters,
+                              validation_duration_fraction=0.5, plots=True)
+
+
+plt.figure()
+plt.plot(phot_results.times, light_curve, 'k.')
+plt.plot(phot_results.times, transit_model_c(phot_results.times), 'r')
+plt.xlabel('Time [JD]')
+plt.ylabel('Flux')
+plt.show()
